4a87f2ced2f009eb0d260966f8ae10ac7b35ba24
hyoom <ghkddbstj@naver.com>
2024-10-20 01:11:26 +0900

    Initial Commit

---
+++ src/main/java/com/ktb/joing/JoingApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.joing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoingApplication.class, args);
+	}
+
+}
