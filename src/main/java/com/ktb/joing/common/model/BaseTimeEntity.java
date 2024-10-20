d9be8e431fe5ab68fc8809acef1a97c6b55cea67
hyoom <ghkddbstj@naver.com>
2024-10-20 22:35:09 +0900

    JOING-8 feat: Entity에 공통적으로 적용하는 BaseTime 엔티티

---
+++ src/main/java/com/ktb/joing/common/model/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.ktb.joing.common.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = true, updatable = true)
+    private LocalDateTime updatedDateTime;
+}
