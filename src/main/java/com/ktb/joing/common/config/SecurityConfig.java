95471607d7355ace0de4c20b733b5f83a4c6adc0
hyoom <ghkddbstj@naver.com>
2024-10-23 14:34:45 +0900

    JOING-18 feat: Security Config 클래스 기본설정

---
+++ src/main/java/com/ktb/joing/common/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.ktb.joing.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //csrf 비횔성화
+        http
+                .csrf((auth) -> auth.disable());
+
+        //From 로그인 방식 비활성화
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //HTTP Basic 인증 방식 비활성화
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        //oauth2 로그인 설정
+        http
+                .oauth2Login(Customizer.withDefaults());
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+
+        //세션 설정 : STATELESS
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
