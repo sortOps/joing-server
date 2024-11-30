32576914749d537e3fe7fde02b6b617b0ddca733
hyoom <ghkddbstj@naver.com>
2024-11-30 15:47:29 +0900

    fix: 회원가입 프론트 경로 Security 설정 추가

--- src/main/java/com/ktb/joing/common/config/SecurityConfig.java
+++ src/main/java/com/ktb/joing/common/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
         http.securityMatcher("/**") // 모든 요청에 대해
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/healthz", "/oauth2/**", "/login/**").permitAll()
-                        .requestMatchers("/api/v1/users/signup/**").hasRole("TEMP_USER")
+                        .requestMatchers("/signup/**", "/api/v1/users/signup/**").hasRole("TEMP_USER")
                         .anyRequest().authenticated()
                 );
 
